694960323b7011c32dfebf47c5f58415e5d89dbe
Emil Madsen <sovende@gmail.com>
2017-08-24 14:34:17 +0200

    Added setup.sh file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Copy vim files
+cp -r $DIR/.vim $HOME/
+cp $DIR/.vimrc $HOME/
+
+# Run PlugInstall
+vim -c "PlugInstall|q|q"
